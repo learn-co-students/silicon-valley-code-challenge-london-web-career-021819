02beaa86c13e4946339a323553f14299b2169eb2
github username <github email address>
2019-02-25 09:56:40 +0000

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,42 @@
 class VentureCapitalist
+  attr_reader :name
+  attr_accessor :total_worth
 
+  @@venture_capitalists = []
+
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth.to_f
+    @@venture_capitalists << self
+  end
+
+  def self.all
+    @@venture_capitalists
+  end
+
+  def self.tres_commas_club
+    self.all.select {|vc| vc.total_worth > 1000000000}
+  end
+
+  def offer_contract(startup, type, amount)
+    FundingRound.new(startup, self, type, amount)
+  end
+
+  def funding_rounds
+    FundingRound.all.select {|fr| fr.vc == self}
+  end
+
+  def portfolio
+    funding_rounds.collect {|fr| fr.startup}.uniq
+  end
+
+  def biggest_investment
+    funding_rounds.max { |a, b| a.investment <=> b.investment }
+  end
+
+  def invested(domain)
+    rounds = funding_rounds.select {|fr| fr.startup.domain == domain}
+    investments = rounds.collect {|fr| fr.investment}
+    investments.sum
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,20 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+top_jobs = Startup.new('Top Jobs', 'Max', 'www.top-jobs.com')
+apple = Startup.new('Apple', 'Steve', 'www.apple.com')
+microsoft = Startup.new('Microsoft', 'Bill', 'www.microsoft.com')
+
+
+james = VentureCapitalist.new('James', 900000)
+russ = VentureCapitalist.new('Russ', 2000000000)
+
+apple_russ = FundingRound.new(apple, russ, 'seed', 100000)
+apple_james = FundingRound.new(apple, james, 'angel', 20000)
+apple_james2 = FundingRound.new(apple, james, 'vulture', 50000)
+apple_russ2 = FundingRound.new(apple, russ, 'angel', 500000)
+microsoft_russ1 = FundingRound.new(microsoft, russ, 'test1', 2000000)
+microsoft_russ1 = FundingRound.new(microsoft, russ, 'test2', 5000000)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
