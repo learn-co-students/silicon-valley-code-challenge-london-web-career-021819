6f166555a4e0c3a9478d1e580c05dc5f3ef335cb
Khang <khang.le27@gmail.com>
2019-02-25 12:54:14 +0000

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,24 @@
 class FundingRound
+  @@all = []
+  attr_reader :startup, :venture_capitalist, :type, :investment
+
+  def self.all
+    @@all
+  end
+
+  def initialize(startup, venture_capitalist, type, investment)
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @type = type
+
+    if investment < 0.0
+      @investment = 0
+    elsif investment >= 0.0
+      @investment = (investment).to_f
+    end
+
+    self.class.all << self
+  end
+
 
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,59 @@
 class VentureCapitalist
 
+  @@all = []
+  attr_accessor :name, :total_worth
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+    self.class.all << self
+  end
+
+  def self.tres_commas_club
+    self.all.select {|venture_capitalist| venture_capitalist.total_worth >= 10**9}
+  end
+
+
+  def offer_contract(startup,type,investment)
+    FundingRound.new(startup, self, type, investment)
+  end
+
+  def funding_rounds
+    funding_rounds = []
+    FundingRound.all.each do |funding_round|
+      funding_rounds << funding_round if funding_round.venture_capitalist == self
+    end
+    funding_rounds
+  end
+
+  def portfolio
+    portfolio = []
+    funding_rounds.each do
+      |funding_round| portfolio << funding_round.startup
+    end
+    portfolio.uniq
+  end
+
+  def biggest_investment
+    max_investment = 0
+    funding_rounds.each do |funding_round|
+      while max_investment < funding_round.investment
+        max_investment = funding_round.investment
+      end
+    end
+    funding_rounds.find {|funding_round| funding_round.investment == max_investment}
+  end
+
+  def invested(domain)
+    total = 0
+    funding_rounds.each do |funding_round|
+      total += funding_round.investment if funding_round.startup.domain == domain
+    end
+    total 
+  end
+
 end
