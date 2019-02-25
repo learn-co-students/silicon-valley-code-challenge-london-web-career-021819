94092cbb4d8e1426ca3b89cb55cc0633f5a6743e
mariamjaludi <mariam.jaludi@gmail.com>
2019-02-25 15:53:18 +0000

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,23 @@
 class FundingRound
 
+  attr_reader :startup, :venture_capitalist, :type, :investment
+  @@all = []
+  def initialize(startup:, venture_capitalist:, type:, investment:)
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @type = type
+    @investment = investment
+    @@all << self
+  end
+
+  def investment
+    if @investment >= 0
+      @investment.to_f
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
 end
