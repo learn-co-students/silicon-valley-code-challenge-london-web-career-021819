8d801fb8dac2ea9e2788e12ca691211843e570e0
github username <github email address>
2019-03-03 10:13:36 +0000

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,18 @@
 class FundingRound
+attr_accessor :startup, :venture_capitalist, :type, :investment
+@@all = []
+
+  def initialize(startup, venture_capitalist, type, investment)
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @type = type
+    raise "Investment can't be negative" unless investment >= 0
+    @investment = investment
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,60 @@
 class Startup
+attr_reader :name, :founder, :domain
+attr_accessor :startup, :type_of_investment
+@@all = []
+@@funding_rounds = []
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pivot=(domain, name)
+    @domain = domain
+    @name = name
+  end
+
+  def pivot(domain, name)
+    @domain = domain
+    @name = name
+  end
+
+  def self.find_by_founder(founder)
+    self.all.find {|startup| startup.founder == founder}
+  end
+
+  def self.domains
+    self.all.map {|project| project.domain}.uniq
+  end
+
+  def sign_contract(venture_capitalist, type_of_investment, ammount)
+    FundingRound.new(self, venture_capitalist, type_of_investment, ammount)
+  end
+
+  def funding_rounds
+    FundingRound.all.select {|rounds| rounds.startup == self}
+  end
+
+  def num_funding_rounds
+    funding_rounds.count
+  end
+
+  def total_funds
+    funding_rounds.map {|rounds| rounds.investment}.inject :+
+  end
+
+  def investors
+    funding_rounds.map {|rounds| rounds.venture_capitalist}.uniq
+  end
+
+  def big_investors
+    investors.select {|investor| VentureCapitalist.tres_commas_club.include?(investor)}
+  end
 
 end
